eebcd944dac5ca6b2d866d0de3b7f333b98247b5
Rudolf Schlenger <schlengerrudolfde@gmail.com>
2026-01-20 13:03:57 +0100

    Add tab-switching to access the pages via buttons

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ import { homepage } from "./homepage.js";
 import { menuPage } from "./menuPage.js";
 import { aboutPage } from "./aboutPage.js";
 
+const homeButton = document.getElementById("home");
+const menuButton = document.getElementById("menu");
+const aboutButton = document.getElementById("about");
+
+homeButton.addEventListener("click", () => {
+    homepage.display();
+})
+
+menuButton.addEventListener("click", () => {
+    menuPage.display();
+})
+
+aboutButton.addEventListener("click", () => {
+    aboutPage.display();
+})
+
 homepage.display();
